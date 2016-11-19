fc39c9d9ddceffdce8be6951c9c81980df4659eb
antego <antego@users.noreply.github.com>
2016-11-19 08:52:07 +0300

    Fix formatting

--- src/main/java/com/github/antego/authorreg/RegisterController.java
+++ src/main/java/com/github/antego/authorreg/RegisterController.java
@@ -1,28 +1,8 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.antego.authorreg;
 
-import java.util.Date;
-import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -31,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 // todo error page
 // todo test
@@ -43,23 +22,23 @@ public class RegisterController {
     @Autowired
     private NamedAccountSerializer serializerService;
 
-	@GetMapping("/")
-	public String registerForm(Model model) {
-		model.addAttribute("copyrightHolder", new CopyrightHolder());
-		return "register";
-	}
+    @GetMapping("/")
+    public String registerForm(Model model) {
+        model.addAttribute("copyrightHolder", new CopyrightHolder());
+        return "register";
+    }
 
-	@PostMapping("/")
-	public HttpEntity<byte[]> registerSubmit(@ModelAttribute CopyrightHolder copyrightHolder) throws JsonProcessingException {
+    @PostMapping("/")
+    public HttpEntity<byte[]> registerSubmit(@ModelAttribute CopyrightHolder copyrightHolder) throws JsonProcessingException {
         NamedAccount account = emaService.getNamedAccount(copyrightHolder.getName());
         byte[] serializedNamedAccount = serializerService.serialize(account);
 
-		HttpHeaders header = new HttpHeaders();
-		header.setContentType(new MediaType("application", "json"));
-		header.set("Content-Disposition",
-				"attachment; filename=keys.json");
-		header.setContentLength(serializedNamedAccount.length);
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "json"));
+        header.set("Content-Disposition",
+                "attachment; filename=keys.json");
+        header.setContentLength(serializedNamedAccount.length);
 
-		return new HttpEntity<>(serializedNamedAccount, header);
-	}
+        return new HttpEntity<>(serializedNamedAccount, header);
+    }
 }
